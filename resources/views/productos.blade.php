ae5a105318459353889cfe11af0eab4488fb4e56
Cristobalam <crisaraos@hotmail.com>
2022-08-04 19:37:22 -0400

    segundo commit

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 8 Datatables Tutorial - ItSolutionStuff.com</title>
+    <title></title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
